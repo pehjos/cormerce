665aec2c7255b0e6e567b88c4f5f48c204c43035
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-17 04:05:11 -0800

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,7 +9,7 @@ export const getPosts = async (req, res) => {
     const { page } = req.query;
     
     try {
-        const LIMIT = 1000;
+        const LIMIT = 10;
         const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
     
         const total = await PostMessage.countDocuments({});
