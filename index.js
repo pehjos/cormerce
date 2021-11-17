072b6408d2e671bb61b3a5ef5aed08eacc2d161a
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-17 06:06:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/order.js"
 const app = express();
 
 
-
+app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use('/posts', postRoutes);
