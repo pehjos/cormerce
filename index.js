54c480e182d2cdc4741fc4fd19a92183a013af5a
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-17 06:19:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import userRouter from "./routes/user.js";
 import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js"
 const app = express();
-
-
 app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -23,15 +21,4 @@ const PORT = process.env.PORT|| 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
-//Cross Origin Handle Middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 
-  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if( req.method === 'OPTIONS'){
-    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+
