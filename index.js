9937b1465b42c2c44dd8f1b72247569fca784dc2
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-17 02:52:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,14 @@ import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js"
 const app = express();
 
-app.use(cors())
-
-
-app.use((req,res, next)=>{
- 
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-  res.setHeader('Access-Control-Allow-Origin',"*");
-  res.setHeader('Access-Control-Allow-Headers',"*");
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use((req, res, next) => 
+{ 
+res.header("Access-Control-Allow-Origin", "*") 
+res.header( "Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept Authorization" ) 
+if (req.method === "OPTIONS") 
+{
+res.header( "Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE" )
+     return res.status(200).json({}) } next() })
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -40,5 +35,5 @@ const CONNECTION_URL = 'mongodb+srv://mall:mall123@cluster0.tccjj.mongodb.net/ma
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+.catch((error) => console.log(`${error} did not connect`));
