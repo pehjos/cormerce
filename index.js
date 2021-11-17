f1498e2d8dbe650b409673bbb89a23ea331c8d8e
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-16 22:10:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ app.use(cors())
 
 
 app.use((req,res, next)=>{
- 
+
     res.header("Access-Control-Allow-Origin", "https://forecastmall.com");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-  res.setHeader('Access-Control-Allow-Origin',"*");
   res.setHeader('Access-Control-Allow-Headers',"*");
   res.header('Access-Control-Allow-Credentials', true);
   next();
