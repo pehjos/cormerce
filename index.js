ff5cb47c2dbbb2de35eb0c1db7100458006283d4
pehjos <71464455+pehjos@users.noreply.github.com>
2021-11-17 06:04:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors';
+
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -9,24 +9,29 @@ import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js"
 const app = express();
 
-app.use( cors({ origin: "http://localhost:3000", credentials: true, }) );
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-
-
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 app.use("/cart",cartRouter);
 app.use("/order",orderRouter);
 // connection staring
-
-
-
 const CONNECTION_URL = 'mongodb+srv://mall:mall123@cluster0.tccjj.mongodb.net/mall?retryWrites=true&w=majority';
-
 const PORT = process.env.PORT|| 5000;
-
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
+//Cross Origin Handle Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if( req.method === 'OPTIONS'){
+    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
